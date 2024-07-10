f70b789af094f558975421b000d4ff69809ba6ef
uuchu <jackjack3912@gmail.com>
2024-07-10 15:31:41 +0900

    無限swiper完成

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,13 @@ var swiper = new Swiper('.mySwiper', {
   },
   allowTouchMove: false,
 });
+
+const endlessSwiper = new Swiper('.endlessSwiper', {
+  loop: true, // ループ有効
+  slidesPerView: 6, // 一度に表示する枚数
+  speed: 6000, // ループの時間
+  allowTouchMove: false, // スワイプ無効
+  autoplay: {
+    delay: 0, // 途切れなくループ
+  },
+});
